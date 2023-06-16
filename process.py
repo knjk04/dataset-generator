0552af640f74eaee378b2e498fc42fc2cd38cea8
Karan Kumar <karan@karankumar.com>
2023-06-16 13:21:11 +0100

    chore: remove redundant column

--- process.py
+++ process.py
@@ -1,25 +1,43 @@
+import logging
+import sys
+
 import pandas as pd
 from dotenv import dotenv_values
 from io import StringIO
 import openai
 
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+FORMAT = "%(asctime)s %(message)s"
+logger = logging.getLogger()
 
-# TODO: Take in what dataset the user wants as input and append to prompt
-def get_response() -> pd.DataFrame:
+
+def get_response(dataset_of: str) -> pd.DataFrame:
     env = dotenv_values(".env")
     openai.api_key = env["OPENAI_API_KEY"]
 
     # TODO: allow users to use a different model
     response = openai.Completion.create(
         model="text-davinci-003",
-        prompt="Generate a table of Harry Potter quotes",
+        prompt=f"Generate a table of {dataset_of}",
         max_tokens=4000,
         temperature=0.2,
     )
 
-    print(f"Tokens used: {response.usage.total_tokens}")
+    logger.debug(f"Tokens used: {response.usage.total_tokens}")
     answer = response.choices[0].text
     print(repr(answer))
     print(answer)
     data = StringIO(answer)
-    return pd.read_csv(data, sep="|")
+    df = pd.read_csv(data, sep="|")
+
+    # The separator also appears at the end, so delete the last redundant column
+    df.drop(df.columns[len(df.columns)-1], axis=1, inplace=True)
+
+    # TODO: drop unnamed columns
+
+    return df
+
+
+def get_models() -> list[str]:
+    # text - davinci - 003, text - davinci - 002, davinci, curie, babbage, ada
+    raise NotImplementedError
