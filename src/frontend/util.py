5e36d1e2075d4fde93c99ad8edbe6980c29fd29b
Karan Kumar <karan@karankumar.com>
2023-06-16 16:55:09 +0100

    chore: cache JSON data

--- src/frontend/util.py
+++ src/frontend/util.py
@@ -1,12 +1,14 @@
 import pandas as pd
 import streamlit as st
 
+
 @st.cache_data
 def df_to_csv(df: pd.DataFrame):
     # cache to avoid reconverting on every run
     return df.to_csv().encode("utf-8")
 
 
+@st.cache_data
 def df_to_json(df: pd.DataFrame):
     # cache to avoid reconverting on every run
     return df.to_json().encode("utf-8")
