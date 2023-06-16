31d9b22873e43c315983e93a25e7cfd551a98704
Karan Kumar <karan@karankumar.com>
2023-06-16 13:48:04 +0100

    chore: show error message if df is empty

--- src/frontend/st_app.py
+++ src/frontend/st_app.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import streamlit as st
+import time
 
 from process import get_response
 
@@ -14,6 +15,7 @@ st.title(app_title)
 
 # TODO: create a numerical box asking for the number of rows
 
+
 with st.form("form"):
     # invalid = True
     dataset = st.text_input(label="What would you like a dataset of?", placeholder="E.g. Harry Potter quotes")
@@ -25,17 +27,21 @@ with st.form("form"):
     generate = st.form_submit_button(label="Generate dataset", disabled=invalid)
 
 if dataset:
-    st.write(f'Generating a dataset of {dataset}...')
+    with st.spinner(f"Generating a dataset of {dataset}..."):
+        time.sleep(5)
 
 if generate and dataset:
     df = get_response(dataset)
-    st.dataframe(df, use_container_width=True)
+    # If dataframe is empty, show an error message and hide the download button
+    if df.empty:
+        st.error(f"We could not generate a {dataset} dataset. Try generating a different dataset.",
+                 icon="🤔")
+    else:
+        st.dataframe(df, use_container_width=True)
 
-    st.download_button(
-        label="Export to CSV",
-        data=df_to_csv(df),
-        file_name="dataset.csv",
-        mime="text/csv"
-    )
-
-
+        st.download_button(
+            label="Export to CSV",
+            data=df_to_csv(df),
+            file_name="dataset.csv",
+            mime="text/csv"
+        )
