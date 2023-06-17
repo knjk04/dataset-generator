ab876cf4ab9d65498edf768767980e88e92454bd
Karan Kumar <knjk04@users.noreply.github.com>
2023-06-17 07:39:57 +0100

    feat: add GPT-3.5 functionality (#9)

--- src/api/process.py
+++ src/api/process.py
@@ -1,5 +1,6 @@
 import logging
 import sys
+from enum import Enum
 
 import pandas as pd
 from dotenv import dotenv_values
@@ -11,24 +12,26 @@ FORMAT = "%(asctime)s %(message)s"
 logger = logging.getLogger()
 
 
+class Models(Enum):
+    # Important for GPT-3.5 to be at the top because this is the default model
+    GPT_3_5 = "gpt-3.5-turbo"
+    DAVINCI = "text-davinci-003"
+
+
 def get_response(dataset_of: str, gpt_choice) -> pd.DataFrame:
     env = dotenv_values(".env")
     openai.api_key = env["OPENAI_API_KEY"]
 
-    # TODO: allow users to use a different model
-    response = openai.Completion.create(
-        model=gpt_choice,
-        prompt=f"Generate a markdown table of {dataset_of}",
-        max_tokens=4000,
-        temperature=0.2,
-    )
-
-    logger.debug(f"Tokens used: {response.usage.total_tokens}")
-    answer = response.choices[0].text
-    print(repr(answer))
+    if gpt_choice == Models.GPT_3_5.value:
+        answer = get_gpt_3_5_response(dataset_of)
+    else:
+        answer = get_da_vinci_response(dataset_of)
     print(answer)
+
     data = StringIO(answer)
     df = pd.read_csv(data, sep="|")
+
+    # TODO: add a function that checks if the response is valid markdown
 
     # The separator also appears at the end, so delete the last redundant column
     df.drop(df.columns[len(df.columns)-1], axis=1, inplace=True)
@@ -40,6 +43,28 @@ def get_response(dataset_of: str, gpt_choice) -> pd.DataFrame:
     return df.iloc[1:]
 
 
+def get_gpt_3_5_response(dataset_of: str) -> str:
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            # Have to say 'make' not 'generate' for GPT 3.5 to work
+            {"role": "user", "content": f"Make a markdown table of {dataset_of}"}
+        ]
+    )
+    return response.choices[0].message.content
+
+
+def get_da_vinci_response(dataset_of: str) -> str:
+    response = openai.Completion.create(
+        model=Models.DAVINCI.value,
+        prompt=f"Generate a markdown table of {dataset_of}",
+        max_tokens=4000,
+        temperature=0.2,
+    )
+    logger.debug(f"Tokens used: {response.usage.total_tokens}")
+    return response.choices[0].text
+
+
 def get_models() -> list[str]:
-    # text-davinci-003, text-davinci-002, davinci, curie, babbage, ada
-    return ["text-davinci-003", "text-davinci-002", "davinci", "curie"]
+    return [m.value for m in Models]
+
