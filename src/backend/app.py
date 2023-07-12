8551896929adfb4140e163b06ed3bd2a2d961156
Karan Kumar <karan@karankumar.com>
2023-07-12 06:41:06 +0100

    chore: configure Flask backend

---
+++ src/backend/app.py
@@ -0,0 +1,20 @@
+from enum import Enum
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+class Models(Enum):
+    # Important for GPT-3.5 to be at the top because this is the default model
+    GPT_3_5 = "gpt-3.5-turbo"
+    DAVINCI = "text-davinci-003"
+
+
+@app.route("/models")
+def get_models() -> list[str]:
+    return [m.value for m in Models]
+
+
+if __name__ == '__main__':
+    app.run()
