acc5a5acf06164a023c458b76d6070d15bc53cb2
Karan Kumar <karan@karankumar.com>
2023-07-24 20:02:41 +0100

    refactor: extract str to df to function

--- src/frontend/api.py
+++ src/frontend/api.py
@@ -31,28 +31,6 @@ class Models(Enum):
     DAVINCI = "text-davinci-003"
 
 
-def get_gpt_3_5_response(dataset_of: str) -> str:
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            # Have to say 'make' not 'generate' for GPT 3.5 to work
-            {"role": "user", "content": f"Make a markdown table of {dataset_of}"}
-        ]
-    )
-    return response.choices[0].message.content
-
-
-def get_da_vinci_response(dataset_of: str) -> str:
-    response = openai.Completion.create(
-        model=Models.DAVINCI.value,
-        prompt=f"Generate a markdown table of {dataset_of}",
-        max_tokens=4000,
-        temperature=0.2,
-    )
-    logger.debug(f"Tokens used: {response.usage.total_tokens}")
-    return response.choices[0].text
-
-
 def get_auth_header(api_key: str):
     return {
         "Authorization": api_key
@@ -60,21 +38,22 @@ def get_auth_header(api_key: str):
 
 
 def get_response(dataset_of: str, gpt_choice, api_key: str) -> pd.DataFrame:
-    headers = get_auth_header(api_key)
     if gpt_choice == Models.GPT_3_5.value:
         url = f"{BASE_URL}/gpt-3.5/{dataset_of}"
-        response = requests.get(url=url, headers=headers)
-        answer = response.text
     else:
-        answer = get_da_vinci_response(dataset_of)
-    print(answer)
+        url = f"{BASE_URL}/davinci/{dataset_of}"
 
-    data = StringIO(answer)
+    headers = get_auth_header(api_key)
+    response = requests.get(url=url, headers=headers)
+    return str_to_df(response.text)
+
+
+# TODO: add a function that checks if the input is valid markdown
+def str_to_df(s: str) -> pd.DataFrame:
+    data = StringIO(s)
     df = pd.read_csv(data, sep="|")
 
-    # TODO: add a function that checks if the response is valid markdown
-
-    # The separator also appears at the end, so delete the last redundant column
+    # The separator is also at the end, so delete the last redundant column
     df.drop(df.columns[len(df.columns)-1], axis=1, inplace=True)
 
     # Remove any unnamed columns
@@ -82,3 +61,4 @@ def get_response(dataset_of: str, gpt_choice, api_key: str) -> pd.DataFrame:
 
     # We remove the first row because it contains the markdown divider ("----")
     return df.iloc[1:]
+
